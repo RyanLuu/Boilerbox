0fa67299200b905495911583fb304b016ddb726a
Ryan Luu <rluu@purdue.edu>
2018-09-18 15:49:45 +0100

    Added mobile route

--- openshift/server.js
+++ openshift/server.js
@@ -12,7 +12,11 @@ let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 let ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
-    res.render('index.html');
+  res.render('index.html');
+});
+
+app.get('/mobile', function(req, res) {
+  res.render('mobile.html');
 });
 
 // error handling
